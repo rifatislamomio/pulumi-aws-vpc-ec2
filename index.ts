e4ee7eadd3458c6f548b109915b415a2d1104478
Rifat <rifat.islam173@northsouth.edu>
2024-06-09 02:46:12 +0600

    vpc configured

--- index.ts
+++ index.ts
@@ -1,19 +1,46 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws";
 
-const vpcA = new awsx.ec2.Vpc("vpc-a", {
+const vpc = new aws.ec2.Vpc("vpc-dev", {
   cidrBlock: "100.0.0.0/16",
-  instanceTenancy: "default",
-  numberOfAvailabilityZones: 1,
-  subnetSpecs: [
-    { type: awsx.ec2.SubnetType.Public, cidrMask: 22, name: "public-subnet-a" },
-    {
-      type: awsx.ec2.SubnetType.Private,
-      cidrMask: 20,
-      name: "private-subnet-a",
-    },
-  ],
+  enableDnsHostnames: true,
+  enableDnsSupport: true,
   tags: {
-    Name: "vpc-a",
-  },
+    Name: "vpc-dev"
+  }
 });
+
+const subnet = new aws.ec2.Subnet("vpc-dev-subnet", {
+  vpcId: vpc.id,
+  cidrBlock: "100.0.1.0/24",
+  mapPublicIpOnLaunch: true,
+  tags: {
+    Name: "vpc-dev-subnet"
+  }
+});
+
+const igw = new aws.ec2.InternetGateway("vpc-dev-igw", {
+  vpcId: vpc.id,
+  tags: {
+    Name: "vpc-dev-igw"
+  }
+});
+
+const routeTable = new aws.ec2.RouteTable("vpc-dev-route-table", {
+  vpcId: vpc.id,
+  routes: [
+    {
+      cidrBlock: "0.0.0.0/0",
+      gatewayId: igw.id
+    }
+  ]
+});
+
+const rtSubnetAssociation = new aws.ec2.RouteTableAssociation(
+  "vpc-dev-rt-subnet-association",
+  {
+    subnetId: subnet.id,
+    routeTableId: routeTable.id
+  }
+);
