7075288fe535d260c470413a0f53fb107a6355d8
Rifat <rifat.islam173@northsouth.edu>
2024-06-10 03:26:52 +0600

    configured ec2 instance

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as awsx from "@pulumi/awsx";
 import * as aws from "@pulumi/aws";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const vpc = new aws.ec2.Vpc("vpc-dev", {
   cidrBlock: "100.0.0.0/16",
@@ -44,3 +45,49 @@ const rtSubnetAssociation = new aws.ec2.RouteTableAssociation(
     routeTableId: routeTable.id
   }
 );
+
+const securityGroup = new aws.ec2.SecurityGroup("ec2-dev-sg", {
+  vpcId: vpc.id,
+  description: "Allowing SSH and HTTP only",
+  ingress: [
+    {
+      protocol: "tcp",
+      fromPort: 22,
+      toPort: 22,
+      cidrBlocks: ["0.0.0.0/0"]
+    },
+    {
+      protocol: "tcp",
+      fromPort: 80,
+      toPort: 80,
+      cidrBlocks: ["0.0.0.0/0"]
+    }
+  ],
+  egress: [
+    {
+      protocol: "-1", // Allow all protocols
+      fromPort: 0,
+      toPort: 0,
+      cidrBlocks: ["0.0.0.0/0"]
+    }
+  ],
+  tags: {
+    Name: "ec2-dev-sg"
+  }
+});
+
+const ec2KeyPair = new aws.ec2.KeyPair("ec2-dev-kp", {
+  keyName: "ec2-dev-kp",
+  publicKey: process.env.PUBLIC_KEY!
+});
+
+const ec2Instance = new aws.ec2.Instance("ec2-dev-instance", {
+  instanceType: "t2.small",
+  ami: "ami-04b70fa74e45c3917", //Ubuntu, 24.04 LTS
+  keyName: ec2KeyPair.keyName,
+  vpcSecurityGroupIds: [securityGroup.id],
+  subnetId: subnet.id,
+  tags: {
+    Name: "ec2-dev-instance"
+  }
+});
